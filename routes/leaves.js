39c1ca87fa64f5f3f30f3de9051f14149821089b
rmwdcsur <risso.santiago@gmail.com>
2026-02-21 23:14:22 -0300

    leaves route

--- routes/leaves.js
+++ routes/leaves.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-// Basic placeholder routes for leaves
-router.get('/', (req, res) => {
-	res.json({ message: 'Leaves route' });
+// Routes for leaves
+router.get("/", (req, res) => {
+  res.json({ message: "Leaves route" });
 });
 
 module.exports = router;
-
